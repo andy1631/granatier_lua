789ee671b59c2577cd35f5eab51fcdb04ba26ab5
mrhappyhof <mrhappyhof@gmail.com>
2020-09-15 13:13:39 +0200

    Map parser works

--- mapParser.lua
+++ mapParser.lua
@@ -5,7 +5,7 @@ Xml = require 'lib.xml2lua.xml2lua'
 MapParser= Class{}
 
 function MapParser:init()
-   self.filePath="resources/arenas/clanbomber_Arena.xml"
+   self.filePath="resources/arenas/granatier.xml"
 end
 
 function MapParser:parse()
@@ -31,6 +31,33 @@ function MapParser:parse()
     for j=1,x,1 do
       local ycord=string.sub(xcord,j,j)
       local temp="arena_ground"
+      if ycord==" " then
+        temp="air"
+      elseif ycord=="=" then
+        temp="arena_greenwall"
+      elseif ycord=="_" then
+        temp="arena_ground"
+      elseif ycord=="+" then
+        temp="arena_wall"
+      elseif ycord=="x" then
+        --50/50
+        temp="arena_ground"
+      elseif ycord=="-" then
+        temp="arena_ice"
+      elseif ycord=="o" then
+        temp="arena_bomb_mortar"
+      elseif ycord=="u" then
+        temp="arena_arrow_up"
+      elseif ycord=="r" then
+        temp="arena_arrow_right"
+      elseif ycord=="d" then
+        temp="arena_arrow_down"
+      elseif ycord=="l" then
+        temp="arena_arrow_left"
+      elseif ycord=="p" then
+        --player poistion
+        temp="arena_ground"
+      end
       map:changeType(j,i,temp)
     end
   end
